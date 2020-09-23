2772d0e88adb901340c7e3dbbd104b387df0fe47
desmarita <desmaritafitriani84@gmail.com>
2020-09-23 21:27:31 +0700

    [W]Login dan Register multi akses menggunakan spatie

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,15 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+
+    protected function authenticated(Request $request, $user)
+    {
+        if ($user->hasRole('admin')){
+            return redirect()->route('dashboard');
+        }
+
+        return redirect()->route('home');
     }
 }
